5e0d7b91adc61d860c2cf1a17247fe2b34aa1a50
Natacha Ivannikova <natacha.im@gmail.com>
2021-03-17 13:24:49 +0100

    ✨ - firebase persistence

--- src/ui/App/Routes/Routes.tsx
+++ src/ui/App/Routes/Routes.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import { Switch, Route, useHistory, Link } from 'react-router-dom';
 import {Login } from '../../pages/Login/Login';
 import {Dashboard } from '../../pages/Dashboard/Dashboard';
 import {Settings } from '../../pages/Settings/Settings';
 import { PrivateRoute } from './PrivateRoute';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {firebase} from '../../firebase/firebase'
+import {setUserLogged} from '../../Store/modules/user/actions'
 
 
 const Routes: React.FC = () => {
@@ -25,15 +27,43 @@ const Routes: React.FC = () => {
   const {isLogged} = useSelector(state => state.user);
   console.log('isLogged: ', isLogged)
 
+  // Escuchamos si el usuario está logueado
   useEffect(() => {
     redirectUser(isLogged);
   }, [isLogged]);
+
+  // Fase de montaje, para leer de firebase y sabrá cuando se monte si el usuario esta logueado o no
+  const dispatch = useDispatch();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        dispatch(setUserLogged(user));
+      }
+    });
+  }, []);
   
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <PrivateRoute exact path="/dashboard" component={Dashboard} />
-      <PrivateRoute exact path="/settings" component={Settings}/>
+      {!isLogged && <Route exact path="/" component={Login} /> }
+      {isLogged && (
+        <>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/settings">Settings</Link>
+              </li>
+              <li>
+                <Link to="/companies">Companies</Link>
+              </li>
+            </ul>
+          </nav>
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute exact path="/settings" component={Settings} />
+        </>
+      )}
     </Switch>
   );
 };
